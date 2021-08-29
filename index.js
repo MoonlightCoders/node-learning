9c99f435728e12f3770bc710b18daade5068ddcb
Gowtham <54016506+gowtham-xseed@users.noreply.github.com>
2021-08-29 12:41:34 +0530

    Port change

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const routes = require('./routes');
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 require('./config/db');
 app.use(routes);
 
 // app.use('api/', routes);
 
-app.listen(3000)
+app.listen(PORT)
